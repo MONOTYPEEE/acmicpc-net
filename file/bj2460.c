d154213379eae5d2835b3be50215b172c795bbcb
monotype <spair0039@gmail.com>
2022-09-21 08:44:11 +0900

    2460

---
+++ file/bj2460.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int max=0,now=0;
+    for(int i=0;i<=9;i++){
+        int in,ot;
+        scanf("%d %d", &ot, &in);
+        now=now+in;
+        now=now-ot;
+        if(now>max){
+            max=now;
+        }
+    }
+    printf("%d",max);
+}
