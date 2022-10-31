8ca28c37e620db0f89d4f91bd0d694dab34549c8
monotypeee <spair0039@gmail.com>
2022-10-31 16:16:10 +0900

    15964

---
+++ bj15964.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    // 30점따리
+    // int aa,bb;
+    // scanf("%d %d",&aa,&bb);
+    // printf("%d",(aa+bb)*(aa-bb));
+
+    //100점
+    long long aa, bb;
+    scanf("%lld %lld",&aa,&bb);
+    printf("%lld",(aa+bb)*(aa-bb));}
